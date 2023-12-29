b62d26d148d793ec23c80a8d2f367aa9ed2e1212
hackbynight <70255003+hackbynight@users.noreply.github.com>
2023-12-29 14:42:30 +0100

    Update OOCBuffs.cs

--- AIO/Combat/Warlock/OOCBuffs.cs
+++ AIO/Combat/Warlock/OOCBuffs.cs
@@ -24,12 +24,12 @@ namespace AIO.Combat.Warlock
         public List<RotationStep> Rotation => new List<RotationStep> 
         {
             new RotationStep(new RotationAction("Consumables Management", ConsumablesManagement), 0f, 5000),
-            new RotationStep(new RotationBuff("Unending Breath"), 2f, (s,t) => !Me.IsMounted, RotationCombatUtil.FindMe),
-            new RotationStep(new RotationBuff("Fel Armor"), 3f, (s, t) => !Me.IsMounted, RotationCombatUtil.FindMe, Exclusive.WarlockSkin),
-            new RotationStep(new RotationBuff("Demon Armor"), 4f, (s, t) => !Me.IsMounted, RotationCombatUtil.FindMe, Exclusive.WarlockSkin),
-            new RotationStep(new RotationBuff("Demon Skin"), 5f, (s, t) => !Me.IsMounted, RotationCombatUtil.FindMe, Exclusive.WarlockSkin),
-            new RotationStep(new RotationBuff("Soul Link"), 6f, (s, t) => !Me.IsMounted, RotationCombatUtil.FindMe),
-            new RotationStep(new RotationBuff("Life Tap"), 7f, (s, t) => !Me.IsMounted && Me.ManaPercentage < 80 && Me.ManaPercentage < Me.HealthPercent && Settings.Current.LifeTapOOC, RotationCombatUtil.FindMe),
+            new RotationStep(new RotationBuff("Unending Breath"), 1f, (s,t) => !Me.IsMounted, RotationCombatUtil.FindMe),
+            new RotationStep(new RotationBuff("Fel Armor"), 2f, (s, t) => !Me.IsMounted, RotationCombatUtil.FindMe, Exclusive.WarlockSkin),
+            new RotationStep(new RotationBuff("Demon Armor"), 3f, (s, t) => !Me.IsMounted, RotationCombatUtil.FindMe, Exclusive.WarlockSkin),
+            new RotationStep(new RotationBuff("Demon Skin"), 4f, (s, t) => !Me.IsMounted, RotationCombatUtil.FindMe, Exclusive.WarlockSkin),
+            new RotationStep(new RotationBuff("Soul Link"), 5f, (s, t) => !Me.IsMounted, RotationCombatUtil.FindMe),
+            new RotationStep(new RotationBuff("Life Tap"), 6f, (s, t) => !Me.IsMounted && Me.ManaPercentage < 80 && Me.ManaPercentage < Me.HealthPercent && Settings.Current.LifeTapOOC, RotationCombatUtil.FindMe),
         };
 
         public void Initialize() { }
